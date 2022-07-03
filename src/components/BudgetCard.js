934c684de11049df4d13e361973410495e730efe
triciachow0212 <suyeechow0212@gmail.com>
2022-07-03 21:38:06 +0800

    BudgetCard template

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
-function BudgetCard() {
-  return <div>BudgetCard</div>;
+function BudgetCard({ name, amount, max }) {
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            <div>{name}</div>
+            <div>
+              {amount} / {max}
+            </div>
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
 
 export default BudgetCard;

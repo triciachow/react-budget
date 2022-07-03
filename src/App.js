bed6eff7b84595bac53211274886834f12a09e99
triciachow0212 <suyeechow0212@gmail.com>
2022-07-03 21:13:42 +0800

    Create header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Button, Container, Stack } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <div>Budget App</div>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary">Add Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        ></div>
+      </Container>
     </>
   );
 }

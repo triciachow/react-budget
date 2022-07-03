d93303f1faf1cc3212469b15da3ec60d5c8338ae
triciachow0212 <suyeechow0212@gmail.com>
2022-07-04 00:28:36 +0800

    Create BudgetContext

--- src/context/BudgetsContext.js
+++ src/context/BudgetsContext.js
@@ -1,5 +1,60 @@
-export function useBudget() {}
+import React, { useContext, useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+
+const BudgetsContext = React.createContext();
+
+export function useBudget() {
+  return useContext(BudgetsContext);
+}
 
 export const BudgetProvider = ({ children }) => {
-  return children;
+  const [budgets, setBudgets] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+
+  function getBudgetExpenses(budgetId) {
+    return expenses.filter(expense => expense.budgetId === budgetId);
+  }
+
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses(prevExpenses => {
+      return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
+    });
+  }
+
+  function addBudget({ name, max }) {
+    setBudgets(prevBudgets => {
+      if (prevBudgets.find(budget => budget.name === name)) {
+        return prevBudgets;
+      }
+      return [...prevBudgets, { id: uuidV4(), name, max }];
+    });
+  }
+
+  function deleteBudget({ id }) {
+    setBudgets(prevBudgets => {
+      return prevBudgets.filter(budget => budget.id !== id);
+    });
+  }
+
+  function deleteExpense({ id }) {
+    setExpenses(prevExpenses => {
+      return prevExpenses.filter(expense => expense.id !== id);
+    });
+  }
+
+  return (
+    <BudgetsContext.Provider
+      value={{
+        budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpense,
+        addBudget,
+        deleteBudget,
+        deleteExpense,
+      }}
+    >
+      {children}
+    </BudgetsContext.Provider>
+  );
 };
